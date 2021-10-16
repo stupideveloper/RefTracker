fa5b7830cf9299c7157da072bdb5b3e924b4fa5e
Lachlan Kemp <me@lachlankemp.com>
2021-10-17 09:29:51 +1030

    refactoring + code commenting

--- server/index.js
+++ server/index.js
@@ -17,50 +17,55 @@ const refPath = '/lkref' // Path for incoming data
 
 const {Create, Collection, Match, Index, Get, Ref, Paginate, Sum, Delete, Add, Select, Let, Var, Update} = faunadb.query;
 
-async function dataIn(request) {
-  const content = await request.json()
-  const referrer = content.referrer;
-  console.log(`Creating ref for ${siteName}. Referrer: ${referrer}`)
-  try {
-    // Create Database Entry
-    const result = await faunaClient.query(
-      Create(
-        Collection(collectionName),
-        {
-          data: {
-            'site': siteName,
-            'siteReferrer': referrer,
-            'cf': request.cf
-          }
-        }
-      )
-    );
-    console.log(`Ref Created for ${siteName}. DB Ref ID: ${result.ref.id}`)
-    // Send Response to user
-    let response = new Response(JSON.stringify({
-      dbref: result.ref.id
-    }))
-    // Set cors headers
-    response.headers.set("Access-Control-Allow-Origin", corsOriginDomain)
-    response.headers.set("Access-Control-Allow-Methods", 'POST,GET')
-    return response
-  } catch (error) {
-    // Handle Errors
-    const faunaError = getFaunaError(error);
-    console.error(faunaError)
-    return new Response(JSON.stringify(faunaError), { status: faunaError.status, headers: { 'Content-type': 'application/json', ...corsHeaders(allowedOrigin) } })
-  }
-}
 
 async function handleRequest(request) {
   const url = new URL(request.url)
-  if(url.pathname === refPath) {
-    dataIn(refPath)
-    return
+
+  if (url.pathname === refPath) {
+    const content = await request.json()
+    const referrer = content.referrer;
+
+    console.log(`Creating ref for ${siteName}. Referrer: ${referrer}`)
+    try {
+      // Create Database Entry
+      const result = await faunaClient.query(
+        Create(
+          Collection(collectionName),
+          {
+            data: {
+              'site': siteName,
+              'siteReferrer': referrer,
+              'cf': request.cf
+            }
+          }
+        )
+      );
+      console.log(`Ref Created for ${siteName}. DB Ref ID: ${result.ref.id}`)
+
+      // Send Response to user
+      let response = new Response(JSON.stringify({
+        dbref: result.ref.id
+      }))
+
+      // Set cors headers
+      response.headers.set("Access-Control-Allow-Origin", corsOriginDomain)
+      response.headers.set("Access-Control-Allow-Methods", 'POST,GET')
+      return response
+      
+    } catch (error) {
+      // Handle Errors
+      const faunaError = getFaunaError(error);
+      console.error(faunaError)
+      
+      // Send Error to user
+      return new Response(JSON.stringify(faunaError), { status: faunaError.status, headers: { 'Content-type': 'application/json', ...corsHeaders(allowedOrigin) } })
+    }
   }
+  // Redirect to home page if root
   if(url.pathname === '/') {
     return Response.redirect(corsOriginDomain, 301)
   }
+  // Return 404 if not found
   return new Response(JSON.stringify({
     error: '404 Not Found'
   }), {
